f3a6ef153e628b8158e56c1a6354c907911b9ff9
Mykyta Uzlov <fullstackly@gmail.com>
2020-05-29 17:23:46 +0300

    fixing engine tests

--- engine/engine_test.go
+++ engine/engine_test.go
@@ -3,24 +3,6 @@ package engine
 import "testing"
 
 func TestEngine(t *testing.T) {
-
-	engineTests := []struct {
-		initial string
-		encoded string
-	}{
-		{initial: "hello", encoded: "h2ll4"},
-		{initial: "hi there", encoded: "h3 th2r2"},
-		{initial: "YOU", encoded: "Y45"},
-		{initial: "Ola", encoded: "4l1"},
-	}
-
-	assertCoding := func(t *testing.T, got, want string) {
-		t.Helper()
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
-	}
-
 	t.Run("encode", func(t *testing.T) {
 		for _, testCase := range engineTests {
 			got := Encode(testCase.initial)
@@ -31,7 +13,26 @@ func TestEngine(t *testing.T) {
 	t.Run("decode", func(t *testing.T) {
 		for _, testCase := range engineTests {
 			got := Decode(testCase.encoded)
-			assertCoding(t, got, testCase.initial)
+			assertCoding(t, got, testCase.decoded)
 		}
 	})
+}
+
+var engineTests = []struct {
+	initial string
+	encoded string
+	decoded string
+}{
+	{initial: "hello", encoded: "h2ll4", decoded: "hello"},
+	{initial: "hi there", encoded: "h3 th2r2", decoded: "hi there"},
+	{initial: "YOU", encoded: "Y45", decoded: "You"},
+	{initial: "Ola", encoded: "4l1", decoded: "ola"},
+}
+
+func assertCoding(t *testing.T, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
