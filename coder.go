8ff1e5f45aa8039819130f66d2b9d4cc1d4cb47f
Mykyta Uzlov <fullstackly@gmail.com>
2020-06-01 12:30:51 +0300

    manage dependencies for CLI

--- coder.go
+++ coder.go
@@ -14,14 +14,14 @@ var (
 
 func main() {
 	flag.Parse()
-	process()
+	process(*text2Encode, *text2Decode)
 }
 
-func process() {
-	if len(*text2Encode) != 0 {
-		fmt.Println(engine.Encode(*text2Encode))
-	} else if len(*text2Decode) != 0 {
-		fmt.Println(engine.Decode(*text2Decode))
+func process(text2Encode, text2Decode string) {
+	if len(text2Encode) != 0 {
+		fmt.Println(engine.Encode(text2Encode))
+	} else if len(text2Decode) != 0 {
+		fmt.Println(engine.Decode(text2Decode))
 	} else {
 		fmt.Println("Enter command and text. \nEnter -h for help")
 	}
